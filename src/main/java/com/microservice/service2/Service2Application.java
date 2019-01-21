96d8631cd1bbba232284eff22fe04fe34018229f
Amrit Krishan <amkrishan@gmail.com>
2019-01-21 15:57:28 +0530

    Employee Info Dashboard Microservice

---
+++ src/main/java/com/microservice/service2/Service2Application.java
@@ -0,0 +1,24 @@
+package com.microservice.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Service2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service2Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+}
+
